6738ff6ddb5729f5f08daecc9c0ecb4248205012
Clif Hodges <chodges@Clifs-MBP.attlocal.net>
2020-08-16 19:34:11 -0500

    ex4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,15 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+    result = []
+    for n in a:
+        hash_table[n] = n
 
+    for n in a:
+        if n < 0:
+            if abs(n) in hash_table:
+                result.append(abs(n))
     return result
 
 
